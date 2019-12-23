debeacfe558fdb279f20206d406c8c27acdc6f80
DESKTOP-P9I81RM\wong <a26bc48@gmail.com>
2019-12-24 00:38:30 +0800

    report function finish

---
+++ src/components/report/Report.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>數據統計</el-breadcrumb-item>
+      <el-breadcrumb-item>數據報表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
+      <div id="main" style="width: 750px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+import _ from 'lodash'
+export default {
+  name: 'Report',
+  data () {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  async mounted () {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'))
+    const { data: res } = await this.$http.get('reports/type/1')
+    console.log(res)
+    if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+    // 指定图表的配置项和数据
+
+    // 合併對象
+    const result = _.merge(res.data, this.options)
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(result)
+  }
+}
+</script>
+
+<style scoped></style>
